ea754343f19cb52dcc70533ca2f64ae601023b23
JamalDE <Jamalahmd820@gmail.com>
2019-08-21 10:10:39 +0500

    RoboFriends Project Files

---
+++ src/robots.js
@@ -0,0 +1,64 @@
+export const robots = [
+{
+	id:1,
+	name: 'James',
+	username: 'Alexx',
+	email: 'Alex@gmail.com'
+},
+{
+	id:2,
+	name: 'Ervin',
+	username: 'Ervi',
+	email: 'Ervin@gmail.com'
+},
+{
+	id:3,
+	name: 'Peter',
+	username: 'Karianne',
+	email: 'Nathan@yesinia.net'
+},
+{
+	id:4,
+	name: 'Patricia Labcak',
+	username: 'Samantha',
+	email: 'Juliana@oconer.org'
+},
+{
+	id:5,
+	name: 'Chelsie Dietrich',
+	username: 'Kamrin',
+	email: 'Locio@htinger.ca'
+},
+{
+	id:6,
+	name: 'Leanne Graham',
+	username: 'Bret',
+	email: 'Sincere@april.biz'
+},
+{
+	id:7,
+	name: 'Graham',
+	username: 'Bre',
+	email: 'Sincere@a.biz'
+},
+{
+	id:8,
+	name: 'Leann',
+	username: 'Lea',
+	email: 're@april.biz'
+},
+{
+	id:9,
+	name: 'Glena',
+	username: 'delphine',
+	email: 'chaim@macdemot.io'
+},
+{
+	id:10,
+	name: 'clementina Deboqeu',
+	username: 'Maria.Setenton',
+	email: 'Rey.padbug@fm'
+}
+]
+
+export default robots;
